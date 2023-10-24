c9afd23889a757a6019c9fce628221a7b496c43a
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-10-24 09:40:13 +0100

    Add group task methods

---
+++ item.rb
@@ -0,0 +1,31 @@
+require 'date'
+
+class Item
+    attr_reader :published_date, :genre, :label, :author
+
+    def initialize(published_date, genre)
+        @id = rand(1..1000)
+        @genre = genre
+        @published_date = published_date
+        @archived = false
+
+    end
+
+    def move_to_archive
+        @archived = can_be_archived?
+    end
+
+    def can_be_archived?
+        return true if
+        years_since_published_date >= 10
+    end
+
+    def years_since_published_date 
+    Date.today.year - @published_date.year
+    end
+
+    def label=(label)
+        @label = label
+        label.add_item(self)
+    end
+end
